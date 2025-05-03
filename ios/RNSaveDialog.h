3ece6179fbe3dc6646dfdcc8bc2fb2264de5fa56
Emmanuel Ikechukwu <emmanueln.nike@gmail.com>
2025-05-04 00:31:55 +0100

    Update RNSaveDialog.h

--- ios/RNSaveDialog.h
+++ ios/RNSaveDialog.h
@@ -1,16 +1,5 @@
-#ifdef RCT_NEW_ARCH_ENABLED
-#import <rnsavedialog/rnsavedialog.h>
-#else
 #import <React/RCTBridgeModule.h>
-#endif
-
 #import <UIKit/UIKit.h>
 
-@interface RNSaveDialog : NSObject <
-#ifdef RCT_NEW_ARCH_ENABLED
-        NativeSaveDialogSpec
-#else
-        RCTBridgeModule
-#endif
-    >
+@interface RNSaveDialog : NSObject <RCTBridgeModule>
 @end
